8fb1465a0998e8e9f750be15fc4648210b05d50d
DearShervin <sherviniranaghideh@gmail.com>
2023-09-06 22:20:54 +0330

    Setting Timeout for Scroll.

--- js/script.js
+++ js/script.js
@@ -220,12 +220,12 @@ function handleTouchEnd() {
       // Swipe left
       currentButtonIndex++;
       buttons[currentButtonIndex].click();
-      pages.scroll(0, 0);
+      setTimeout(() => window.scroll(0, 0), 100);
     } else if (touchEndX > touchStartX && currentButtonIndex > 0) {
       // Swipe right
       currentButtonIndex--;
       buttons[currentButtonIndex].click();
-      pages.scroll(0, 0);
+      setTimeout(() => window.scroll(0, 0), 100);
     }
   }
 }
